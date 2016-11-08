af71e145b316ee98f55de34a0a39308a43649b94
imran <imufun1@gmail.com>
2016-11-08 15:04:27 +0600

    add

---
+++ app/src/main/java/shop/portonics/com/shop/BackgroundTask.java
@@ -0,0 +1,45 @@
+package shop.portonics.com.shop;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * Created by imran on 11/8/2016.
+ */
+
+public class BackgroundTask extends AsyncTask<String, Void, Void> {
+
+    Context context;
+
+    public BackgroundTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String url = "http://localhost:8080/apidone/DbAdapter.php";
+        String method = params[0];
+        if (method.equals("register")) {
+            
+        }
+
+        return null;
+    }
+
+
+    @Override
+    protected void onProgressUpdate(Void... params) {
+        super.onProgressUpdate();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
+
--- app/src/main/java/shop/portonics/com/shop/RegisterActivity.java
+++ app/src/main/java/shop/portonics/com/shop/RegisterActivity.java
@@ -6,21 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.HashMap;
-import java.util.Map;
+import static shop.portonics.com.shop.R.id.IdName;
 
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText mName, mAddress, mcity, mCountry, mEmail, mPass, mZip, mPhone;
+    String Name, Address, city, Country, Email, Pass, Zip, Phone;
     Button button;
     RequestQueue requestQueue;
     private static String URL_REGISTER = "http://localhost:8080/apidone/DbAdapter.php";
@@ -32,7 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
         AppAPIs appAPIs = new AppAPIs();
 
 
-        mName = (EditText) findViewById(R.id.IdName);
+        mName = (EditText) findViewById(IdName);
         mAddress = (EditText) findViewById(R.id.IdAddress);
         mcity = (EditText) findViewById(R.id.IdCity);
         mCountry = (EditText) findViewById(R.id.IdCountry);
@@ -40,47 +35,26 @@ public class RegisterActivity extends AppCompatActivity {
         mPass = (EditText) findViewById(R.id.IdPass);
         mZip = (EditText) findViewById(R.id.IdZip);
         mPhone = (EditText) findViewById(R.id.IdPhone);
-        
+
         button = (Button) findViewById(R.id.btnInsert);
 
 
         requestQueue = Volley.newRequestQueue(this);
+    }
 
+    public void RegUser(View view) {
+        Name = mName.getText().toString();
+        Address = mAddress.getText().toString();
+        city = mcity.getText().toString();
+        Country = mCountry.getText().toString();
+        Email = mEmail.getText().toString();
+        Pass = mPass.getText().toString();
+        Zip = mZip.getText().toString();
+        Phone = mPhone.getText().toString();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGISTER,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                System.out.println(response.toString());
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+        String method = "register";
 
-                    }
-                }) {
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> parameters = new HashMap<String, String>();
-                        parameters.put("name", mName.getText().toString());
-                        parameters.put("address", mAddress.getText().toString());
-                        parameters.put("city", mcity.getText().toString());
-                        parameters.put("Country", mCountry.getText().toString());
-                        parameters.put("email", mEmail.getText().toString());
-                        parameters.put("password", mPass.getText().toString());
-                        parameters.put("zip", mZip.getText().toString());
-                        parameters.put("phone", mPhone.getText().toString());
-                        return parameters;
-
-                    }
-                };
-                // Add the request to the RequestQueue.
-                requestQueue.add(stringRequest);
-            }
-        });
-
+        BackgroundTask backgroundTask  =new BackgroundTask(this);
+        backgroundTask.execute(method,Name,Address,city,Country,Email,Pass,Zip,Phone);
     }
 }
