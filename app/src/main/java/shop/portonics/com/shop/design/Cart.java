35ac9ed55f64ea8631a0b2f1d2a19aa3e2204d92
imufun <imufun1@gmail.com>
2016-11-17 23:18:32 +0600

    add cart layout

---
+++ app/src/main/java/shop/portonics/com/shop/design/Cart.java
@@ -0,0 +1,15 @@
+package shop.portonics.com.shop.design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import shop.portonics.com.shop.R;
+
+public class Cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
